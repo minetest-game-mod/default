7bb6d84e63ade117e2ea1b01e8791c571376332d
Alexander Chibrikin <alek13.me@gmail.com>
2022-12-05 05:47:41 +0300

    `default/init`: no use `mapgen`, `aliases`, `legacy`. Relates to lord-server/lord#267, lord-server/lord#601

--- init.lua
+++ init.lua
@@ -63,6 +63,10 @@ dofile(default_path.."/tools.lua")
 dofile(default_path.."/item_entity.lua")
 dofile(default_path.."/craftitems.lua")
 dofile(default_path.."/crafting.lua")
-dofile(default_path.."/mapgen.lua")
-dofile(default_path.."/aliases.lua")
-dofile(default_path.."/legacy.lua")
+-- обновлён, но не подключаем, т.к. генерация своя(убогая, но какая досталась), пока нет возможности обновить её.
+-- генерация перенесена в `mods/lord/_overwrites/MTG/default/mapgen.lua`
+--dofile(default_path.."/mapgen.lua")
+-- добавились, но пока что не подключаем, т.к. есть аналогичные наши в нашем моде legacy
+--dofile(default_path.."/aliases.lua")
+-- обновлён, не подключаем, т.к. у нас свой целый мод legacy, а всё, что в этом файле не используется
+--dofile(default_path.."/legacy.lua")
